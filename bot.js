203fdd65665571a0c5537d8f8bdcb896bfe4b389
Dillon Monday <dillon.g@bresnan.net>
2018-07-03 10:56:37 -0600

    added repeat timer

--- bot.js
+++ bot.js
@@ -5,13 +5,16 @@ var Bot = new TwitterBot(config);
 var doigURL = 'https://arc.lib.montana.edu/ivan-doig/item/';
 var exponentURL = 'http://arc.lib.montana.edu/msu-exponent/item/';
 var homeCookingURL = 'http://arc.lib.montana.edu/book/home-cooking-history-409/item/';
-var exponentSize = 3899;
+var montananURL = 'https://arc.lib.montana.edu/msu-yearbooks/item/';
+var troutURL = 'https://arc.lib.montana.edu/trout-art/item/';
+/*var exponentSize = 3899;
 var doigSize = 6459;
 var cookingSize = 39;
-var collectionNumber = Math.floor(Math.random() * Math.floor(2));
+var montananSize= 91;*/
+var collectionNumber = Math.floor(Math.random() * Math.floor(4));
 
 
-function pickCollection(){
+function pickCollection(){ 			//uses random number to pick between the available collections
 	
 	
 	
@@ -21,27 +24,57 @@ function pickCollection(){
 		return exponentURL;
 	else if (collectionNumber==2)
 		return homeCookingURL;
+	else if(collectionNumber == 3)
+		return montananURL;
+	else if (collectionNumber ==4)
+		return troutURL;
 	
 }
-function getSize(URL) {
+
+function getSize(URL) {				//gets the size of the chosen collection
 	
 
 	
 	if(URL==doigURL)
-		return doigSize;
+		return 6459;
 	else if(URL == exponentURL)
-		return exponentSize;
+		return 3899;
 	else if(URL == homeCookingURL)
-		return cookingSize;
+		return 39;
+	else if (URL == montananURL)
+		return 91;
+	else if (URL == troutURL)
+		return 224;
 }
 
 function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));				//uses the collection size to pick a random item from that collection
 }
 
+function getHashtaglist(collection){							//adds hashtags to the tweet
+	var hashtags = '#MSULibrary #specialcollections #digitalcollections';
+	if(collection==doigURL)
+		hashtags = hashtags +' #doig #ivandoig ';
+	else if(collection == exponentURL)
+		hashtags = hashtags +' #MSUExponent #MSUhistory #studentlife  ';
+	else if(collection == homeCookingURL)
+		hashtags = hashtags +' #recipe #homecooking  ';
+	else if (collection == montananURL)
+		hashtags = hashtags +' #MSUhistory #studentlife ';
+	else if (collection == troutURL)
+		hashtags = hashtags+ ' #trout ';
+	hashtags = collection + hashtags;
+	return hashtags;
+}
+
+function sendTweet() { 						// the main function that prepares and then sends the tweet
 var URL = pickCollection();
 var size = getSize(URL);
 var tweetURL= URL+getRandomInt(size);
- 
+var hashtags = getHashtaglist(URL);
+tweetURL = tweetURL +hashtags;
  Bot.tweet(tweetURL);
  console.log(tweetURL);
+ 
+}
+setInterval(sendTweet, 21600000); //post a random tweet from the collections once every 6 hours
--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 
 module.exports = {
 	consumer_key:'YOUR_CONSUMER_KEY_HERE',
-	consumer_secret:'YOUR_CONSUMER_SECRET_HERE',
+	consumer_secret:'YOUR_CONSUMER_SECRET_KEY_HERE',
 	access_token:'YOUR_ACCESS_TOKEN_HERE',
 	access_token_secret:'YOUR_ACCESS_TOKEN_SECRET_HERE',
 	
